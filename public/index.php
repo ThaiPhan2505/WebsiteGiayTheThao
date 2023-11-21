058db9ae366bbdd52715a9159d38a0ee12531629
PhanThai2505 <pqthai.it.fe@gmail.com>
2023-11-21 23:19:42 +0700

    add file index.php

---
+++ public/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cửa Hàng Giày Thể Thao PVT</title>
+    <link rel="stylesheet" href="../assets/font/fontawesome-free-5.15.4-web/css/all.min.css">
+    <link rel="stylesheet" href="../assets/font/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div id="app">
+        <div class="container-fluid">
+            <div id="header" class="row">
+                <div class="header__logo col">
+                    <a href="#"><img class="header__logo--img img-fluid" src="../images/logo.png" alt="Logo"></a>
+                </div>
+                <div class="header__total col-8">
+                    <ul class="header__total--list">
+                        <li><a href="#" class="text-decoration-none">Giày Nam</a></li>
+                        <li><a href="#" class="text-decoration-none">Giày Nữ</a></li>
+                        <li><a href="#" class="text-decoration-none">Chính Sách Bảo Hành</a></li>
+                        <li><a href="#" class="text-decoration-none">Thông Tin Cửa Hàng</a>
+                        </li>
+                        <li class="">
+                            <a href="#" class="text-decoration-none">Đăng Nhập</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="header__search col-3">
+                    <a href="#">
+                        <button class="header__search--btn">
+                            <i class="ti-search header__search--icon"></i>
+                        </button>
+                    </a>
+                    <input class="header__search--input" type="text" placeholder="Bạn Muốn Tìm Kiếm Gì ?">
+                </div>
+            </div>
+        </div>
+        <div id="body">
+    
+        </div>
+        <div id="footer">
+    
+        </div>
+    </div>
+</body>
+</html>
