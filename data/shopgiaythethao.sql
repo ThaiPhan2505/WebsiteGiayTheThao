5e078b4ebbdf66acb48583d9f3286a8a765dc9fa
Phuc1597 <dtphuc_21th@student.agu.edu.vn>
2023-11-23 15:56:02 +0700

    Create shopgiaythethao.sql

---
+++ data/shopgiaythethao.sql
@@ -0,0 +1,279 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 23, 2023 at 09:55 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shopgiaythethao`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `danhmuc`
+--
+
+CREATE TABLE `danhmuc` (
+  `id` int(11) NOT NULL,
+  `tendanhmuc` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diachi`
+--
+
+CREATE TABLE `diachi` (
+  `id` int(11) NOT NULL,
+  `id_nguoidung` int(11) NOT NULL,
+  `diachi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `giay`
+--
+
+CREATE TABLE `giay` (
+  `id` int(11) NOT NULL,
+  `tengiay` varchar(100) NOT NULL,
+  `id_thuonghieu` int(11) NOT NULL,
+  `mota` text NOT NULL,
+  `id_danhmuc` int(11) NOT NULL,
+  `gianhap` decimal(10,2) NOT NULL,
+  `giaban` decimal(10,2) NOT NULL,
+  `danhgia` int(1) NOT NULL,
+  `hinhanh` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hoadon`
+--
+
+CREATE TABLE `hoadon` (
+  `id` varchar(50) NOT NULL,
+  `id_nguoidung` int(11) NOT NULL,
+  `id_diachi` int(11) NOT NULL,
+  `ngaylap` date NOT NULL,
+  `tongtien` decimal(10,2) NOT NULL,
+  `ghichu` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hoadonchitiet`
+--
+
+CREATE TABLE `hoadonchitiet` (
+  `id` int(11) NOT NULL,
+  `id_hoadon` varchar(50) NOT NULL,
+  `id_giay` int(11) NOT NULL,
+  `dongia` decimal(10,2) NOT NULL,
+  `soluong` int(11) NOT NULL,
+  `size` tinyint(2) NOT NULL,
+  `thanhtien` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nguoidung`
+--
+
+CREATE TABLE `nguoidung` (
+  `id` int(11) NOT NULL,
+  `tennguoidung` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `matkhau` varchar(255) NOT NULL,
+  `sdt` varchar(11) NOT NULL,
+  `loai` tinyint(1) NOT NULL,
+  `trangthai` tinyint(1) NOT NULL,
+  `hinhanh` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `size_soluong`
+--
+
+CREATE TABLE `size_soluong` (
+  `id` int(11) NOT NULL,
+  `id_giay` int(11) NOT NULL,
+  `size` tinyint(2) NOT NULL,
+  `soluong` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thuonghieu`
+--
+
+CREATE TABLE `thuonghieu` (
+  `id` int(11) NOT NULL,
+  `tenthuonghieu` int(100) NOT NULL,
+  `diachi` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `sdt` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `danhmuc`
+--
+ALTER TABLE `danhmuc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `diachi`
+--
+ALTER TABLE `diachi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `diachi_nguoidung` (`id_nguoidung`);
+
+--
+-- Indexes for table `giay`
+--
+ALTER TABLE `giay`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `giay_thuonghieu` (`id_thuonghieu`),
+  ADD KEY `giay_danhmuc` (`id_danhmuc`);
+
+--
+-- Indexes for table `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `hoadon_nguoidung` (`id_nguoidung`),
+  ADD KEY `hoadon_diachi` (`id_diachi`);
+
+--
+-- Indexes for table `hoadonchitiet`
+--
+ALTER TABLE `hoadonchitiet`
+  ADD KEY `hoadonct_hoadon` (`id_hoadon`),
+  ADD KEY `hoadonct_giay` (`id_giay`);
+
+--
+-- Indexes for table `nguoidung`
+--
+ALTER TABLE `nguoidung`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `size_soluong`
+--
+ALTER TABLE `size_soluong`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sizesoluong_giay` (`id_giay`);
+
+--
+-- Indexes for table `thuonghieu`
+--
+ALTER TABLE `thuonghieu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `danhmuc`
+--
+ALTER TABLE `danhmuc`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `diachi`
+--
+ALTER TABLE `diachi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `giay`
+--
+ALTER TABLE `giay`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nguoidung`
+--
+ALTER TABLE `nguoidung`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `size_soluong`
+--
+ALTER TABLE `size_soluong`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `thuonghieu`
+--
+ALTER TABLE `thuonghieu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `diachi`
+--
+ALTER TABLE `diachi`
+  ADD CONSTRAINT `diachi_nguoidung` FOREIGN KEY (`id_nguoidung`) REFERENCES `nguoidung` (`id`);
+
+--
+-- Constraints for table `giay`
+--
+ALTER TABLE `giay`
+  ADD CONSTRAINT `giay_danhmuc` FOREIGN KEY (`id_danhmuc`) REFERENCES `danhmuc` (`id`),
+  ADD CONSTRAINT `giay_thuonghieu` FOREIGN KEY (`id_thuonghieu`) REFERENCES `thuonghieu` (`id`);
+
+--
+-- Constraints for table `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD CONSTRAINT `hoadon_diachi` FOREIGN KEY (`id_diachi`) REFERENCES `diachi` (`id`),
+  ADD CONSTRAINT `hoadon_nguoidung` FOREIGN KEY (`id_nguoidung`) REFERENCES `nguoidung` (`id`);
+
+--
+-- Constraints for table `hoadonchitiet`
+--
+ALTER TABLE `hoadonchitiet`
+  ADD CONSTRAINT `hoadonct_giay` FOREIGN KEY (`id_giay`) REFERENCES `giay` (`id`),
+  ADD CONSTRAINT `hoadonct_hoadon` FOREIGN KEY (`id_hoadon`) REFERENCES `hoadon` (`id`);
+
+--
+-- Constraints for table `size_soluong`
+--
+ALTER TABLE `size_soluong`
+  ADD CONSTRAINT `sizesoluong_giay` FOREIGN KEY (`id_giay`) REFERENCES `giay` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
