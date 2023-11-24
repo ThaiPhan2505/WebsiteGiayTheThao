2ef6b1d9ca5faec2e5e17edd76ee4bc7fa159274
ThuyVi <94538117+ThuyVi2505@users.noreply.github.com>
2023-11-24 22:20:36 +0700

    Update index.php add link bootstrap 5

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cửa Hàng Giày Thể Thao PVT</title>
+    <link rel="typesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
   
