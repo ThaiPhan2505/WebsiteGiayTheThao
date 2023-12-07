6aaa5acd35e3a5c1dbef117c72fd006457278e43
PhanThai2505 <pqthai.it.fe@gmail.com>
2023-12-07 17:08:59 +0700

    update id of form dangnhap

--- public/index.php
+++ public/index.php
@@ -240,7 +240,7 @@
 </div>
 
 <!-- Model Đăng nhập -->
-<div class="modal fade" id="#modalDangnhap" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade" id="modalDangnhap" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
