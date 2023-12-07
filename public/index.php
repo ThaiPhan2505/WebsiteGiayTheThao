b7ab7c56be4f25c18aba526e00e367c88b954102
PhanThai2505 <pqthai.it.fe@gmail.com>
2023-12-07 17:07:11 +0700

    update form dangnhap

--- public/index.php
+++ public/index.php
@@ -98,7 +98,7 @@
                 </div>
                 <div class="header__login col">
                     <div class="header__login--total">
-                    <div class="" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    <div class="" data-bs-toggle="modal" data-bs-target="#modalDangky">
                         <a href="#" class="text-decoration-none"><i class="ti-user header__login--icon"></i></a>
                     </div>
                         
@@ -201,100 +201,71 @@
     </div>
 
 
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">ĐĂNG KÝ TÀI KHOẢN</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <form>
-
-<div class="form-outline mb-4">
-    <label class="form-label" for="form3Example1cg">Tên của bạn</label>
-    <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
-</div>
-
-<div class="form-outline mb-4">
-    <label class="form-label" for="form3Example4cg">Mật khẩu</label>
-    <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
-</div>
-
-<div class=" mb-4">
-    <label class="form-label" for="form3Example4cdg">Nhập lại mật khẩu</label>
-    <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
-</div>
-
-<div class="form-check d-flex justify-content-center mb-5">
-    <label class="form-check-label" for="form2Example3g">
-    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
-    Tôi đồng ý với các <a href="#!" class="text-body"><u>Điều khoản của dịch vụ</u></a>
-</label>
-</div>
-
-</form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-success d-flex">Đăng Ký</button>
-      </div>
-      <p class="text-center text-muted mt-5 mb-3">Bạn đã có tài khoản? <a href="#!" class="fw-bold text-body"><u>Mời đăng nhập</u></a></p>
-    </div>
-  </div>
-</div>
-
-    <!-- <section class="vh-100 bg-image header__login-regis modal fade " id="staticBackdrop"
-        style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
-        <div class="mask d-flex align-items-center h-100 gradient-custom-3 modal mw-100">
-            <div class="container h-100 ">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-                <div class="card" style="border-radius: 15px;">
-                    <div class="card-body p-5">
-                    <h2 class="text-uppercase text-center mb-5">TẠO TÀI KHOẢN MỚI</h2>
-
-                    <form>
-
-                        <div class="form-outline mb-4">
-                            <label class="form-label" for="form3Example1cg">Tên của bạn</label>
-                            <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
-                        </div>
-
-                        <div class="form-outline mb-4">
-                            <label class="form-label" for="form3Example4cg">Mật khẩu</label>
-                            <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
-                        </div>
-
-                        <div class="form-outline mb-4">
-                            <label class="form-label" for="form3Example4cdg">Nhập lại mật khẩu</label>
-                            <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
-                        </div>
-
-                        <div class="form-check d-flex justify-content-center mb-5">
-                            <label class="form-check-label" for="form2Example3g">
+<!-- Modal Đăng ký-->
+<div class="modal fade" id="modalDangky" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">ĐĂNG KÝ TÀI KHOẢN</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="form3Example1cg">Tên của bạn</label>
+                        <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                    </div>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="form3Example4cg">Mật khẩu</label>
+                        <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                    </div>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="form3Example4cdg">Nhập lại mật khẩu</label>
+                        <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
+                    </div>
+                    <div class="form-check d-flex justify-content-center mb-5">
+                        <label class="form-check-label" for="form2Example3g">
                             <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
                             Tôi đồng ý với các <a href="#!" class="text-body"><u>Điều khoản của dịch vụ</u></a>
                         </label>
-                        </div>
-
-                        <div class="d-flex justify-content-center">
-                        <button type="button"
-                            class="btn btn-success btn-block btn-lg gradient-custom-4 text-body ">Đăng Ký</button>
-                        </div>
-
-                        <p class="text-center text-muted mt-5 mb-0">Bạn đã có tài khoản? <a href="#!"
-                            class="fw-bold text-body"><u>Mời đăng nhập</u></a></p>
-
-                    </form>
-
                     </div>
-                </div>
-                </div>
+                </form>
             </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success d-flex">Đăng Ký</button>
+            </div>
+            <p class="text-center text-muted mt-5 mb-3">Bạn đã có tài khoản? <a href="#!" class="fw-bold text-body" data-bs-toggle="modal" data-bs-target="#modalDangnhap"><u>Mời đăng nhập</u></a></p>
+        </div>
+    </div>
+</div>
+
+<!-- Model Đăng nhập -->
+<div class="modal fade" id="#modalDangnhap" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">ĐĂNG NHẬP</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="form3Example1cg">Tên của bạn</label>
+                        <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                    </div>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="form3Example4cg">Mật khẩu</label>
+                        <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success d-flex">Đăng Nhập</button>
             </div>
         </div>
-    </section> -->
+    </div>
+</div>
+    
     
 </body>
 </html>
