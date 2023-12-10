36098d3c444dc4b6b27179449f35ef64ddb299c3
Phuc1597 <dtphuc_21th@student.agu.edu.vn>
2023-12-10 23:24:20 +0700

    Update nguoidung.php

---
+++ model/nguoidung.php
@@ -0,0 +1,143 @@
+<?php
+class NGUOIDUNG{
+    private $id;
+    private $tennguoidung;
+    private $email;
+    private $matkhau;
+    private $sdt;
+    private $loai;
+    private $trangthai;
+    private $hinhanh;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTennguoidung()
+    {
+        return $this->tennguoidung;
+    }
+
+    public function setTennguoidung($tennguoidung): self
+    {
+        $this->tennguoidung = $tennguoidung;
+
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMatkhau()
+    {
+        return $this->matkhau;
+    }
+
+    public function setMatkhau($matkhau): self
+    {
+        $this->matkhau = $matkhau;
+
+        return $this;
+    }
+
+    public function getSdt()
+    {
+        return $this->sdt;
+    }
+
+    public function setSdt($sdt): self
+    {
+        $this->sdt = $sdt;
+
+        return $this;
+    }
+
+    public function getLoai()
+    {
+        return $this->loai;
+    }
+
+    public function setLoai($loai): self
+    {
+        $this->loai = $loai;
+
+        return $this;
+    }
+
+    public function getTrangthai()
+    {
+        return $this->trangthai;
+    }
+
+    public function setTrangthai($trangthai): self
+    {
+        $this->trangthai = $trangthai;
+
+        return $this;
+    }
+
+    public function getHinhanh()
+    {
+        return $this->hinhanh;
+    }
+
+    public function setHinhanh($hinhanh): self
+    {
+        $this->hinhanh = $hinhanh;
+
+        return $this;
+    }
+
+    //Lấy danh sách người dùng
+    public function layDanhSachNguoiDung(){
+        $conn = new DATABASE();
+        $dbconn = $conn->connect();
+        try{
+            $query = "SELECT * FROM nguoidung ORDER BY id DESC";
+            $cmd = $dbconn->prepare($query);
+            $cmd->execute();
+            $kq = $cmd->fetchAll();
+            return $kq;
+        }
+        catch(PDOException $ex){
+            echo 'Lỗi: ' . $ex->getMessage();
+            exit();
+        }
+    }
+    // Lấy người dùng theo id
+    public function layGiayNguoiDung($id){
+        $conn = new DATABASE();
+        $dbconn = $conn->connect();
+        try{
+            $query = "SELECT * FROM nguoidung WHERE id=:id";
+            $cmd = $dbconn->prepare($query);
+            $cmd->bindValue(":id", $id);
+            $cmd->execute();
+            $kq = $cmd->fetch();
+            return $kq;
+        }
+        catch(PDOException $ex){
+            echo 'Lỗi: ' . $ex->getMessage();
+            exit();
+        }
+        return true;
+    }
+}
+?>
