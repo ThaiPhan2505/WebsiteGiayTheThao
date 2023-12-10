84a756d4ccd1df12aa3a90ed73a13fd33f1feec5
PhanThai2505 <pqthai.it.fe@gmail.com>
2023-12-10 21:25:44 +0700

    update product for index.php

--- public/index.php
+++ public/index.php
@@ -84,7 +84,7 @@
             </div>
         </div>
         <div id="body">
-            <div class="banner mt-8">
+            <div class="banner mb-5">
                 <div id="body__banner" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-indicators">
                         <button type="button" data-bs-target="#body__banner" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -110,6 +110,100 @@
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
+                    </div>
+                </div>
+            </div>
+            <div style="background-color: #eee;">
+                <div class="text-center container py-5">
+                    <h4 class="mt-4 mb-5"><strong>Sản Phẩm</strong></h4>
+                    <div class="row">
+                        <div class="col-lg-4 col-md-12 mb-4">
+                            <div class="card">
+                                <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light"
+                                    data-mdb-ripple-color="light">
+                                    <img src="../images/Product/giay1.jpg"
+                                    class="w-50" />
+                                    <a href="#!">
+                                    <div class="mask">
+                                        <div class="d-flex justify-content-start align-items-end h-100">
+                                        <h5><span class="badge bg-primary ms-2">Mới</span></h5>
+                                    </div>
+                                    </div>
+                                    <div class="hover-overlay">
+                                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                    </div>
+                                    </a>
+                                </div>
+                                <div class="card-body">
+                                    <a href="" class="text-reset">
+                                    <h5 class="card-title mb-3">Adidas Untraboost</h5>
+                                    </a>
+                                    <a href="" class="text-decoration-none">
+                                    <p>Giày mang êm chân</p>
+                                    </a>
+                                    <h6 class="mb-3">1.500.000 đ</h6>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 mb-4">
+                            <div class="card">
+                                <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light"
+                                    data-mdb-ripple-color="light">
+                                    <img src="../images/Product/giay2.jpg"
+                                    class="w-50" />
+                                    <a href="#!">
+                                    <div class="mask">
+                                        <div class="d-flex justify-content-start align-items-end h-100">
+                                        <h5><span class="badge bg-primary ms-2">Mới</span></h5>
+                                        </div>
+                                    </div>
+                                    <div class="hover-overlay">
+                                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                    </div>
+                                    </a>
+                                </div>
+                                <div class="card-body">
+                                    <a href="" class="text-reset">
+                                    <h5 class="card-title mb-3">Adidas ABC</h5>
+                                    </a>
+                                    <a href="" class="text-reset">
+                                    <p>Giày mang êm chân</p>
+                                    </a>
+                                    <h6 class="mb-3">2.500.000 đ</h6>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6 mb-4">
+                            <div class="card">
+                                <div class="bg-image hover-zoom ripple" data-mdb-ripple-color="light">
+                                    <img src="../images/Product/giay3.jpg"
+                                    class="w-50" />
+                                    <a href="#!">
+                                    <div class="mask">
+                                        <div class="d-flex justify-content-start align-items-end h-100">
+                                        <h5><span class="badge bg-danger ms-2">-10%</span></h5>
+                                        </div>
+                                    </div>
+                                    <div class="hover-overlay">
+                                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                    </div>
+                                    </a>
+                                </div>
+                                <div class="card-body">
+                                    <a href="" class="text-reset">
+                                    <h5 class="card-title mb-3">Adidas Mega Pull</h5>
+                                    </a>
+                                    <a href="" class="text-reset">
+                                    <p>Giày mang êm chân</p>
+                                    </a>
+                                    <h6 class="mb-3">
+                                    <s>2.000.000 đ</s><strong class="ms-2 text-danger">1.750.000 đ</strong>
+                                    </h6>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
