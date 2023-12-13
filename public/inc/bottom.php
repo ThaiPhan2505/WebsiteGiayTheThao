f8b8502c0a4728945991e3d22c602f3d887040d2
PhanThai2505 <pqthai.it.fe@gmail.com>
2023-12-13 14:19:14 +0700

    insert bottom.php

---
+++ public/inc/bottom.php
@@ -0,0 +1,162 @@
+<div id="footer" class="footer">
+            <div class="grid">
+                <div class="grid__row">
+                    <div class="grid__column-2-4">
+                        <h3 class="footer__heading">Chăm Sóc Khách Hàng</h3>
+                        <ul class="footer__heading-list">
+                            <li class="footer__heading-item">
+                                <a href="" class="footer__heading-link">Trung Tâm Trợ Giúp</a>
+                            </li>
+                            <li class="footer__heading-item">
+                                <a href="" class="footer__heading-link">Trả Hàng - Hoàn Tiền</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="grid__column-2-4">
+                        <h3 class="footer__heading">Thông Tin Về Cửa Hàng</h3>
+                        <ul class="footer__heading-list">
+                            <li class="footer__heading-item">
+                                <a href="#" class="footer__heading-link">Giới Thiệu Cửa Hàng</a>
+                            </li>
+                            <li class="footer__heading-item">
+                                <a href="#" class="footer__heading-link">Điều Khoản Người Dùng</a>
+                            </li>
+                            <li class="footer__heading-item">
+                                <a href="#" class="footer__heading-link">Chính Sách Bảo Hành</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="grid__column-3-4">
+                        <h3 class="footer__heading">Tổng Đài Hổ Trợ</h3>
+                        <ul class="footer__heading-list">
+                            <li class="footer__heading-item">
+                                <a href="" class="footer__heading-link">SĐT Kỹ Thuật: 0325411287</a>
+                            </li>
+                            <li class="footer__heading-item">
+                                <a href="" class="footer__heading-link">Tổng Đài Hổ Trợ Khẩn Cấp: 09321254</a>
+                            </li>
+                            <li class="footer__heading-item">
+                                <a href="" class="footer__heading-link">Tổng Đài Xử Lý Sự Cố: 0369987451</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="grid__column-1-4">
+                        <h3 class="footer__heading">Theo Dõi</h3>
+                        <ul class="footer__heading-list">
+                            <li class="footer__heading-item">
+                                <a href="" class="footer__heading-link">
+                                    <i class="footer__heading-icon-face fab fa-facebook"></i>
+                                    Facebook
+                                </a>
+                            </li>
+                            <li class="footer__heading-item">
+                                <a href="" class="footer__heading-link">
+                                    <i class="footer__heading-icon-insta fab fa-instagram"></i>
+                                    Instagram
+                                </a>
+                            </li>
+                            <li class="footer__heading-item">
+                                <a href="" class="footer__heading-link">
+                                    <i class="footer__heading-icon-yout ti-youtube"></i>
+                                    Youtube
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="grid__column-2-4 footer--dow">
+                        <h3 class="footer__heading">Tải ứng dụng trên :</h3>
+                        <div class="footer__heading-download">
+                            <a href="">
+                                <img src="../../images/Brand/QR_Code.png" alt="" class="footer__heading-download-qr">
+                            </a>
+                            <div class="footer__heading-download-apps">
+                                <a href="">
+                                    <img src="../../images/Logo/ch-play.png" alt="" class="footer__heading-img">
+                                </a>
+                                <a href="">
+                                    <img src="../../images/Logo/app-store.png" alt="" class="footer__heading-img">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="footer__bottom">
+                <div class="grid">
+                    <p class="footer__copyright">© 2023 - Bản Quyền Thuộc Công Ty Giày PVT - Chi Nhánh Việt Nam.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<!-- Modal Đăng ký-->
+<div class="modal fade" id="modalDangky" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">ĐĂNG KÝ TÀI KHOẢN</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="form3Example1cg">Tên của bạn</label>
+                        <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                    </div>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="form3Example4cg">Mật khẩu</label>
+                        <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                    </div>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="form3Example4cdg">Nhập lại mật khẩu</label>
+                        <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
+                    </div>
+                    <div class="form-check d-flex justify-content-center mb-5">
+                        <label class="form-check-label" for="form2Example3g">
+                            <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
+                            Tôi đồng ý với các <a href="#!" class="text-body"><u>Điều khoản của dịch vụ</u></a>
+                        </label>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success d-flex">Đăng Ký</button>
+            </div>
+            <p class="text-center text-muted mt-5 mb-3">Bạn đã có tài khoản? <a href="#!" class="fw-bold text-body" data-bs-toggle="modal" data-bs-target="#modalDangnhap"><u>Mời đăng nhập</u></a></p>
+        </div>
+    </div>
+</div>
+
+<!-- Model Đăng nhập -->
+<div class="modal fade" id="modalDangnhap" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">ĐĂNG NHẬP</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="form3Example1cg">Tên của bạn</label>
+                        <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                    </div>
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="form3Example4cg">Mật khẩu</label>
+                        <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success d-flex">Đăng Nhập</button>
+            </div>
+            <p class="text-center text-muted mt-5 mb-3">Bạn chưa có tài khoản? <a href="#!" class="fw-bold text-body" data-bs-toggle="modal" data-bs-target="#modalDangky"><u>Mời đăng ký tài khoản</u></a></p>
+        </div>
+    </div>
+</div>
+    
+    
+</body>
+</html>
