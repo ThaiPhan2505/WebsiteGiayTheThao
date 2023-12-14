960d744c889e283f8e436eaa1699c1634f15061c
PhanThai2505 <pqthai.it.fe@gmail.com>
2023-12-14 10:58:20 +0700

    update sql

--- data/shopgiaythethao.sql
+++ data/shopgiaythethao.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 13, 2023 lúc 07:52 AM
+-- Thời gian đã tạo: Th12 14, 2023 lúc 04:54 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.2.4
 
@@ -18,15 +18,23 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
-<<<<<<< HEAD
 -- Cơ sở dữ liệu: `shopgiaythethao`
-=======
--- Database: `shopgiaythethao`
-DROP DATABASE IF EXISTS `shopgiaythethao`;
-CREATE DATABASE IF NOT EXISTS `shopgiaythethao` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `shopgiaythethao`;
->>>>>>> 6b4d31a67ffa8593a4b37fc998203ea2d8862c99
 --
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `binhluan`
+--
+
+CREATE TABLE `binhluan` (
+  `id` int(11) NOT NULL,
+  `id_nguoidung` int(11) NOT NULL,
+  `id_giay` int(11) NOT NULL,
+  `ngayphanhoi` date NOT NULL,
+  `noidung` text NOT NULL,
+  `danhgia` enum('1','2','3','4','5') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -50,18 +58,6 @@ INSERT INTO `danhmuc` (`id`, `tendanhmuc`) VALUES
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `diachi`
---
-
-CREATE TABLE `diachi` (
-  `id` int(11) NOT NULL,
-  `id_nguoidung` int(11) NOT NULL,
-  `diachi` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Cấu trúc bảng cho bảng `giay`
 --
 
@@ -74,8 +70,20 @@ CREATE TABLE `giay` (
   `gianhap` decimal(10,2) NOT NULL,
   `giagoc` decimal(10,2) NOT NULL,
   `giaban` decimal(10,2) NOT NULL,
-  `danhgia` int(1) NOT NULL,
   `hinhanh` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `giohang`
+--
+
+CREATE TABLE `giohang` (
+  `id` int(11) NOT NULL,
+  `id_nguoidung` int(11) NOT NULL,
+  `id_giay` int(11) NOT NULL,
+  `soluong` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -87,7 +95,7 @@ CREATE TABLE `giay` (
 CREATE TABLE `hoadon` (
   `id` varchar(50) NOT NULL,
   `id_nguoidung` int(11) NOT NULL,
-  `id_diachi` int(11) NOT NULL,
+  `diachi` varchar(200) NOT NULL,
   `ngaylap` date NOT NULL,
   `tongtien` decimal(10,2) NOT NULL,
   `ghichu` text NOT NULL
@@ -159,44 +167,32 @@ CREATE TABLE `thuonghieu` (
 --
 
 INSERT INTO `thuonghieu` (`id`, `tenthuonghieu`, `diachi`, `email`, `sdt`, `hinhanh`) VALUES
-(1, 'adidas', 'Đức', 'adidas@mail.com', '0156324484', ''),
-(2, 'Nike', 'Hoa Kỳ', 'nike@gmail.com', '0123456789', ''),
-(3, 'Gucci', 'Italia', 'gucci@gmail.com', '0902548763', ''),
-(4, 'Converse', 'Hoa Kỳ', 'converse@gmail.com', '0325874698', ''),
-(5, 'MLB', 'Hàn Quốc', 'MLB@gmail.com', '0231569845', ''),
-(6, 'New Balance', 'Boston, Massachusetts, Hoa Kỳ', 'newbalance@gmail.com', '0254631254', ''),
-(7, 'Puma', 'Herzogenaurach, Bavaria, Đức', 'puma@gmail.com', '0125749611', ''),
+(1, 'Adidas', 'Đức', 'adidas@mail.com', '0156324484', 'images/Brand/Adidas-removebg-preview.png'),
+(2, 'Nike', 'Hoa Kỳ', 'nike@gmail.com', '0123456789', 'images/Brand/Nike.png'),
+(3, 'Gucci', 'Italia', 'gucci@gmail.com', '0902548763', 'images/Brand/Gucci-removebg-preview.png'),
+(4, 'Converse', 'Hoa Kỳ', 'converse@gmail.com', '0325874698', 'images/Brand/Conv-removebg-preview.png'),
+(5, 'MLB', 'Hàn Quốc', 'MLB@gmail.com', '0231569845', 'images/Brand/MLB-removebg-preview.png'),
+(6, 'New Balance', 'Boston, Massachusetts, Hoa Kỳ', 'newbalance@gmail.com', '0254631254', 'images/Brand/NBL.png'),
+(7, 'Puma', 'Herzogenaurach, Bavaria, Đức', 'puma@gmail.com', '0125749611', 'images/Brand/Puma.png'),
 (8, 'Reebok', 'Bolton, Lancashire, Hoa Kỳ', 'reebok@gmail.com', '0125846952', '');
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `y_kien`
---
-
-CREATE TABLE `y_kien` (
-  `id` int(11) NOT NULL,
-  `id_nguoidung` int(11) NOT NULL,
-  `ngayphanhoi` date NOT NULL,
-  `noidung` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Chỉ mục cho các bảng đã đổ
 --
 
 --
+-- Chỉ mục cho bảng `binhluan`
+--
+ALTER TABLE `binhluan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `phanhoi_nguoidung` (`id_nguoidung`),
+  ADD KEY `phanhoi_giay` (`id_giay`);
+
+--
 -- Chỉ mục cho bảng `danhmuc`
 --
 ALTER TABLE `danhmuc`
   ADD PRIMARY KEY (`id`);
-
---
--- Chỉ mục cho bảng `diachi`
---
-ALTER TABLE `diachi`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `diachi_nguoidung` (`id_nguoidung`);
 
 --
 -- Chỉ mục cho bảng `giay`
@@ -207,12 +203,19 @@ ALTER TABLE `giay`
   ADD KEY `giay_danhmuc` (`id_danhmuc`);
 
 --
+-- Chỉ mục cho bảng `giohang`
+--
+ALTER TABLE `giohang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `giohang_giay` (`id_giay`),
+  ADD KEY `giohang_nguoidung` (`id_nguoidung`);
+
+--
 -- Chỉ mục cho bảng `hoadon`
 --
 ALTER TABLE `hoadon`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `hoadon_nguoidung` (`id_nguoidung`),
-  ADD KEY `hoadon_diachi` (`id_diachi`);
+  ADD KEY `hoadon_nguoidung` (`id_nguoidung`);
 
 --
 -- Chỉ mục cho bảng `hoadonchitiet`
@@ -241,15 +244,14 @@ ALTER TABLE `thuonghieu`
   ADD PRIMARY KEY (`id`);
 
 --
--- Chỉ mục cho bảng `y_kien`
---
-ALTER TABLE `y_kien`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `phanhoi_nguoidung` (`id_nguoidung`);
-
---
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
+
+--
+-- AUTO_INCREMENT cho bảng `binhluan`
+--
+ALTER TABLE `binhluan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT cho bảng `danhmuc`
@@ -258,15 +260,15 @@ ALTER TABLE `danhmuc`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT cho bảng `diachi`
---
-ALTER TABLE `diachi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT cho bảng `giay`
 --
 ALTER TABLE `giay`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `giohang`
+--
+ALTER TABLE `giohang`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -288,20 +290,15 @@ ALTER TABLE `thuonghieu`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT cho bảng `y_kien`
---
-ALTER TABLE `y_kien`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Các ràng buộc cho bảng `diachi`
+-- Các ràng buộc cho bảng `binhluan`
 --
-ALTER TABLE `diachi`
-  ADD CONSTRAINT `diachi_nguoidung` FOREIGN KEY (`id_nguoidung`) REFERENCES `nguoidung` (`id`);
+ALTER TABLE `binhluan`
+  ADD CONSTRAINT `phanhoi_giay` FOREIGN KEY (`id_giay`) REFERENCES `giay` (`id`),
+  ADD CONSTRAINT `phanhoi_nguoidung` FOREIGN KEY (`id_nguoidung`) REFERENCES `nguoidung` (`id`);
 
 --
 -- Các ràng buộc cho bảng `giay`
@@ -311,10 +308,16 @@ ALTER TABLE `giay`
   ADD CONSTRAINT `giay_thuonghieu` FOREIGN KEY (`id_thuonghieu`) REFERENCES `thuonghieu` (`id`);
 
 --
+-- Các ràng buộc cho bảng `giohang`
+--
+ALTER TABLE `giohang`
+  ADD CONSTRAINT `giohang_giay` FOREIGN KEY (`id_giay`) REFERENCES `giay` (`id`),
+  ADD CONSTRAINT `giohang_nguoidung` FOREIGN KEY (`id_nguoidung`) REFERENCES `nguoidung` (`id`);
+
+--
 -- Các ràng buộc cho bảng `hoadon`
 --
 ALTER TABLE `hoadon`
-  ADD CONSTRAINT `hoadon_diachi` FOREIGN KEY (`id_diachi`) REFERENCES `diachi` (`id`),
   ADD CONSTRAINT `hoadon_nguoidung` FOREIGN KEY (`id_nguoidung`) REFERENCES `nguoidung` (`id`);
 
 --
@@ -329,12 +332,6 @@ ALTER TABLE `hoadonchitiet`
 --
 ALTER TABLE `size_soluong`
   ADD CONSTRAINT `sizesoluong_giay` FOREIGN KEY (`id_giay`) REFERENCES `giay` (`id`);
-
---
--- Các ràng buộc cho bảng `y_kien`
---
-ALTER TABLE `y_kien`
-  ADD CONSTRAINT `phanhoi_nguoidung` FOREIGN KEY (`id_nguoidung`) REFERENCES `nguoidung` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
