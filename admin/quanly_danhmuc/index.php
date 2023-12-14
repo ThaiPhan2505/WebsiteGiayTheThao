369de7b0afd419dec38217fe477f86bbf829486c
Phuc1597 <dtphuc_21th@student.agu.edu.vn>
2023-12-14 22:14:28 +0700

    update quanlythuonghieu

--- admin/quanly_danhmuc/index.php
+++ admin/quanly_danhmuc/index.php
@@ -1,16 +1,16 @@
 <?php
-    if(isset($_REQUEST["action"])){
-        $action = $_REQUEST["action"];
-    }
-    else{   // mặc định là xem danh sách
-        $action="xem";
-    }
+if(isset($_REQUEST["action"])){
+    $action = $_REQUEST["action"];
+}
+else{   // mặc định là xem danh sách
+    $action="xem";
+}
 
-    switch($action){
-        case "xem":
-            include("main.php");
-            break;
-        default:
-            break;
-    }
+switch($action){
+    case "xem":
+        include("main.php");
+        break;
+    default:
+        break;
+}
 ?>
--- admin/quanly_thuonghieu/index.php
+++ admin/quanly_thuonghieu/index.php
@@ -1,16 +1,20 @@
 <?php
-    if(isset($_REQUEST["action"])){
-        $action = $_REQUEST["action"];
-    }
-    else{   // mặc định là xem danh sách
-        $action="xem";
-    }
+require("../../model/database.php");
+require("../../model/thuonghieu.php");
+if(isset($_REQUEST["action"])){
+    $action = $_REQUEST["action"];
+}
+else{   // mặc định là xem danh sách
+    $action="xem";
+}
+$th = new THUONGHIEU();
 
-    switch($action){
-        case "xem":
-            include("main.php");
-            break;
-        default:
-            break;
-    }
+switch($action){
+    case "xem":
+        $thuonghieu = $th->layDanhSachThuongHieu();
+        include("main.php");
+        break;
+    default:
+        break;
+}
 ?>
